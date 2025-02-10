6b82656a5c83e82f8a4d0d802aec6e14165aa02f
brucelee352 <brucelee352@gmail.com>
2025-02-09 23:23:59 -0500

    More changes to app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
+import os
+from portfolio_app.scripts.constants import DBT_PROFILES_DIR
 from portfolio_app.app import main
+
+# Set environment variables before anything else
+os.environ['DBT_PROFILES_DIR'] = str(DBT_PROFILES_DIR)
 
 if __name__ == "__main__":
     main()
