be6f3f437f5da014b65ab72f6f28bd6a11568dc6
brucelee352 <brucelee352@gmail.com>
2025-02-11 18:40:27 -0500

    Update main_data_pipeline.py

--- portfolio_app/scripts/main_data_pipeline.py
+++ portfolio_app/scripts/main_data_pipeline.py
@@ -106,7 +106,7 @@ def minio_client():
             MINIO_ENDPOINT,
             access_key=MINIO_ROOT_USER,
             secret_key=MINIO_ROOT_PASSWORD,
-            secure=MINIO_USE_SSL
+            secure=False
         )
         LOG.info("Connected to %s", MINIO_ENDPOINT)
         return client
