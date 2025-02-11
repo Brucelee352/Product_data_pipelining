182d3a23c6660b9e4afaac8a2ea70edadd37fe6e
brucelee352 <brucelee352@gmail.com>
2025-02-11 17:58:13 -0500

    Update app.py

--- portfolio_app/app.py
+++ portfolio_app/app.py
@@ -240,7 +240,7 @@ def render_revenue_charts(con):
                     size='total_revenue',
                     hover_data=['product_name', 'total_revenue', 'avg_price'],
                     color='price_tier',
-                    title='Purchase Analysis by Product and Price Tier',
+                    title='Purchases by Product and Pricing',
                     opacity=0.7,
                     log_x=True,
                     width=500,
